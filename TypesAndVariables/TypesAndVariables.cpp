203e8ec0387d5f0dd5b715a0a901fc77864c4ef1
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-10 15:11:23 +0300

    Add TypesAndVariables

---
+++ TypesAndVariables/TypesAndVariables.cpp
@@ -0,0 +1,33 @@
+// TypesAndVariables.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+namespace mine {
+    unsigned int uint = 100;
+}
+
+int main()
+{
+    cout << "Hello World!\n";
+    int num = 5;
+    bool isTrue = 0;
+    double dNum = 12.23123124123;
+    float fNum = 21.12312312312f; // To define float number you must add 'f' to the end of the number
+    char symbol = '#';
+    unsigned int uint = -1; // Unsigned int will overflow - can not be negative
+
+    cout << uint << " - " << mine::uint << endl; // Here we can use different namespaces for variables with the same name
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
