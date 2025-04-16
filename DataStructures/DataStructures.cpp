be6ed1816fe3ca2547a3723c6e663cd8aff6e297
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-16 16:39:15 +0300

    Practice Lists

--- DataStructures/DataStructures.cpp
+++ DataStructures/DataStructures.cpp
@@ -3,42 +3,80 @@
 
 #include <iostream>
 #include<vector>
+#include<list>
 using namespace std;
 
 int main()
 {
-    vector<string> cars = { "Bentli", "Bugati", "Porsche" };
+	// Vectors
+	{
+		vector<string> cars = { "Bentley", "Bugatti", "Porsche" };
 
-    cout << "------Vectors-------" << endl;
-    cout << "First element: " << cars.front() << endl;
-    cout << "Last element: " << cars.back() << endl;
-    cout << "[1] element: " << cars.at(1) << endl;
-    //cout << "Throw element: " << cars.at(12) << endl;
+		cout << "------Vectors-------" << endl;
+		cout << "First element: " << cars.front() << endl;
+		cout << "Last element: " << cars.back() << endl;
+		cout << "[1] element: " << cars.at(1) << endl;
+		//cout << "Throw element: " << cars.at(12) << endl;
 
-    cars.push_back("Tesla");    // Push element into vector 
-    cars.push_back("Tesla");    // Push element into vector 
+		cars.push_back("Tesla");    // Push element into vector 
+		cars.push_back("Tesla");    // Push element into vector 
 
-    for (string car : cars) {
-        cout << "Car brand: " << car << endl;
-    }
-    cars.pop_back();            // Remove last element from vector
-    cars.pop_back();            // Remove last element from vector
-    cars.pop_back();            // Remove last element from vector
+		for (string car : cars) {
+			cout << "Car brand: " << car << endl;
+		}
+		cars.pop_back();            // Remove last element from vector
+		cars.pop_back();            // Remove last element from vector
+		cars.pop_back();            // Remove last element from vector
 
-    cout << "Vector size: " << cars.size() << endl; // Check vector size/length
+		cout << "Vector size: " << cars.size() << endl; // Check vector size/length
 
-    if (cars.empty())       // Check if the vector is empty
-    {
-        cout << "The vector is empty" << endl;
-    }
-    else 
-    {
-        cout << "The vector is not empty" << endl;
-    }
+		if (cars.empty())       // Check if the vector is empty
+		{
+			cout << "The vector is empty" << endl;
+		}
+		else
+		{
+			cout << "The vector is not empty" << endl;
+		}
 
-    for (int i = 0; i < cars.size(); i++) {
-        cout << "Car brand: " << cars.at(i) << endl;
-    }
+		for (int i = 0; i < cars.size(); i++) {
+			cout << "Car brand: " << cars.at(i) << endl;
+		}
+
+		cout << endl;
+		cout << "------Lists-------" << endl;
+	}
+
+	// Lists
+	{
+		list<string> cars = { "Bentley", "Bugatti", "Porsche" };
+
+		cout << "First car: " << cars.front() << endl;
+		cout << "Last car: " << cars.back() << endl;
+
+		cars.front() = "Lamborghini";
+		cars.back() = "Tesla";
+
+		cars.push_front("Bentley");
+		cars.push_back("Porsche");
+
+		cars.pop_front();
+		cars.pop_back();
+
+		for (string car : cars) {
+			cout << "Car brand: " << car << endl;
+		}
+
+		cout << "Size of list: " << cars.size() << endl;
+
+		if (cars.empty()) {
+			cout << "No cars at the list!" << endl;
+		}
+		else {
+			cout << cars.size() << " cars at the list!" << endl;
+		}
+
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
