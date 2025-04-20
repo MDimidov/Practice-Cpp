da6874420bf6efdc5a9f4a60de99ed816ba569f4
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-20 15:25:50 +0300

    create Rock paper scisor game

---
+++ RockPaperScissor/RockPaperScissor.cpp
@@ -0,0 +1,93 @@
+// RockPaperScissor.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+//#include<random>
+#include<cstdlib>
+#include<conio.h>
+using namespace std;
+
+enum Game {
+	Rock = 0,
+	Paper = 1,
+	Scissors = 2,
+	WrongInput = 3,
+};
+
+void printLine() {
+	cout << "----------------------------------------" << endl;
+}
+
+int main()
+{
+	srand(time(0));
+	int randNum = -1;
+	char key;
+	
+	Game playerChoose;
+	Game computerChoose;
+
+	while (true)
+	{
+		randNum = rand() % 3;
+		cout << "Press [R], [P], [S] for [R]ock, [P]aper, [S]cissors. Pres [ESC] for Exit." << endl;
+		key = _getch();
+
+		//cout << key << endl;
+
+		if ((int)key == 27) {
+			printLine();
+			cout << "Good game. See you next time!" << endl;
+			printLine();
+			break;
+		}
+
+		switch (key)
+		{
+		case 'r':
+		case 'R':
+			playerChoose = Rock;
+			cout << "Your choose is Rock." << endl;
+			break;
+		case 'p':
+		case 'P':
+			playerChoose = Paper;
+			cout << "Your choose is Paper." << endl;
+			break;
+		case 's':
+		case 'S':
+			playerChoose = Scissors;
+			cout << "Your choose is Scissors." << endl;
+			break;
+		default:
+			playerChoose = WrongInput;
+			printLine();
+			cout << "You choose wrong input please try again!" << endl;
+			printLine();
+			continue;
+			break;
+		}
+
+		computerChoose = (Game)randNum;
+		cout << "Computer choose is: " << (computerChoose == Rock ? "Rock" : computerChoose == Paper ? "Paper" : "Scissors") << endl;
+
+		printLine();
+		if (playerChoose == computerChoose) {
+			cout << "Game is Draw" << endl;
+		}
+		else if ((playerChoose == Rock && computerChoose == Scissors) ||
+			(playerChoose == Paper && computerChoose == Rock) ||
+			(playerChoose == Scissors && computerChoose == Paper)) 
+		{
+			cout << "The winner is You my friend. Congratulations!" << endl;
+		}
+		else
+		{
+			cout << "You Loose, Computer wins!" << endl;
+		}
+
+		printLine();
+		cout << endl;
+
+	}
+}
