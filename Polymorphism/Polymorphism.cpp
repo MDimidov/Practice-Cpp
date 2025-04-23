62bcb624692f63d38cb8a342dc04a816ec0490e2
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-23 12:41:52 +0300

    Practice Run time polymorphism

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -4,6 +4,56 @@
 #include <iostream>
 #include<vector>
 using namespace std;
+
+class Box {
+public:
+	Box() {}
+
+	Box(int h, int l) {
+		high = h;
+		length = l;
+	}
+
+	void setHigh(int newHigh) {
+		high = newHigh;
+	}
+
+	void setLength(int newLength) {
+		length = newLength;
+	}
+
+	virtual void printTest() {
+		cout << "Box" << endl;
+	}
+
+	void print()
+	{
+		cout << "Box high: " << high << endl;
+		cout << "Box length: " << length << endl;
+	}
+
+protected:
+	int high{ 0 };
+	int length{ 0 };
+};
+
+
+class SuperBox : public Box
+{
+public:
+	SuperBox() {}
+	void print()
+	{
+		cout << "SuperBox high: " << high << endl;
+		cout << "SuperBox length: " << length << endl;
+	}
+
+	void printTest() override {
+		cout << "SuperBox" << endl;
+	}
+
+};
+
 // Compile time polymorphism
 // Functional overloading
 void test() {
@@ -15,21 +65,29 @@ void test(int a) {
 }
 
 void test(int a, double b) {
-    
+
 }
 
 // Operator overloading
-ostream& operator<<(ostream& out, const vector<int>& data) 
+ostream& operator<<(ostream& out, const vector<int>& data)
 {
-    for (int i = 0; i < data.size(); i++) {
-        if (i != 0) {
-            out << ", ";
-        }
+	for (int i = 0; i < data.size(); i++) {
+		if (i != 0) {
+			out << ", ";
+		}
 
-        out << data.at(i);
-    }
+		out << data.at(i);
+	}
 
-    return out;
+	return out;
+}
+
+vector<int>& operator+=(vector<int>& lhs, vector<int>& rhs) {
+	for (auto item : rhs) {
+		lhs.push_back(item);
+	}
+
+	return lhs;
 }
 
 // Run time polymorphism
@@ -37,14 +95,51 @@ ostream& operator<<(ostream& out, const vector<int>& data)
 
 int main()
 {
-    vector<int> items;
-    items.push_back(1);
-    items.push_back(2);
-    items.push_back(3);
-    items.push_back(4);
-    items.push_back(5);
+	{
+		vector<int> items;
+		items.push_back(1);
+		items.push_back(2);
+		items.push_back(3);
+		items.push_back(4);
+		items.push_back(5);
+		cout << items << endl;
 
-    cout << items << endl;
+		vector<int> items2;
+		items2.push_back(6);
+		items2.push_back(7);
+		items2.push_back(8);
+		items2.push_back(9);
+		items2.push_back(0);
 
-    return 0;
+		items += items2;
+		cout << items << endl;
+	}
+
+	{
+		vector<Box> boxes;
+		Box box(4, 12);
+		SuperBox superBox;
+		boxes.push_back(box);
+		boxes.push_back(superBox);
+
+		for (auto& it : boxes) {
+			it.printTest();		// RunTime Polymorphism
+			it.print();
+		}
+	}
+	cout << "--------------------" << endl;
+	{
+		vector<Box*> boxes;
+		Box box(4, 12);
+		SuperBox superBox;
+		boxes.push_back(&box);
+		boxes.push_back(&superBox);
+
+		for (auto& it : boxes) {
+			it->printTest();	// RunTime Polymorphism
+			it->print();
+		}
+	}
+
+	return 0;
 }
