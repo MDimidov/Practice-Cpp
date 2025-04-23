174870d8e3eb925de4d0d0c81d02091a04950a7f
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-23 10:51:39 +0300

    Create project Polymorphism

---
+++ Polymorphism/Polymorphism.cpp
@@ -0,0 +1,16 @@
+// Polymorphism.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+// Compile time polymorphism
+// Functional overloading
+// Operator overloading
+
+// Run time polymorphism
+// Virtual functions
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
