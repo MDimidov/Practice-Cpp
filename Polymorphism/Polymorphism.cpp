578d73c719620c80b9968deffe93f2c5fbea73cf
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-23 11:02:22 +0300

    Practice Functional overloading & Operator overloading

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -2,15 +2,49 @@
 //
 
 #include <iostream>
-
+#include<vector>
+using namespace std;
 // Compile time polymorphism
 // Functional overloading
+void test() {
+
+}
+
+void test(int a) {
+
+}
+
+void test(int a, double b) {
+    
+}
+
 // Operator overloading
+ostream& operator<<(ostream& out, const vector<int>& data) 
+{
+    for (int i = 0; i < data.size(); i++) {
+        if (i != 0) {
+            out << ", ";
+        }
+
+        out << data.at(i);
+    }
+
+    return out;
+}
 
 // Run time polymorphism
 // Virtual functions
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<int> items;
+    items.push_back(1);
+    items.push_back(2);
+    items.push_back(3);
+    items.push_back(4);
+    items.push_back(5);
+
+    cout << items << endl;
+
+    return 0;
 }
