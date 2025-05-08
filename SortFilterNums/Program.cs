6b9628da0d2ff2abbd450d453bf1f0a50fb9c627
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-08 15:56:30 +0300

    Pracitce task for interview

---
+++ SortFilterNums/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> list = Console.ReadLine()!
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+
+printList(removeNegativeAndSort(list));
+
+List<int> removeNegativeAndSort(List<int> ints)
+    => ints.Where(num => num >= 0)
+    .OrderBy(num => num)
+    .ToList();
+
+void printList(List<int> ints)
+{
+    Console.WriteLine(string.Join(", ", ints));
+}
