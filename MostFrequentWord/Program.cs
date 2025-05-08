2c1127d516233e5822de3365c35e2231d2dd72f9
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-08 17:17:20 +0300

    Solved MostFrequent word for interview

---
+++ MostFrequentWord/Program.cs
@@ -0,0 +1,38 @@
+﻿char[] splitters = new[] { ' ', '.', ',', '!', '?', '\"' };
+
+string[] wordsToCheck = Console.ReadLine()!
+    .Split(splitters, StringSplitOptions.RemoveEmptyEntries)
+    .Select(s => s.ToLower())
+    .ToArray();
+
+string[] stopWords = Console.ReadLine()!
+    .Split(splitters, StringSplitOptions.RemoveEmptyEntries)
+    .Select(s => s.ToLower())
+    .ToArray();
+
+
+Dictionary<string, int> result = new();
+
+foreach (string word in wordsToCheck)
+{
+
+    if (!stopWords.Contains(word))
+    {
+        if (!result.ContainsKey(word))
+        {
+            result[word] = 0;
+        }
+
+        result[word]++;
+    }
+}
+
+result = result
+    .OrderByDescending(w => w.Value)
+    .ToDictionary();
+
+foreach (var keyValuePair in result)
+{
+    Console.WriteLine(keyValuePair.Key);
+    break;
+}
