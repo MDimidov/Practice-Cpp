25d28a1afe16f8df66e4775cd7eb60536201d457
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-08 15:41:01 +0300

    Practice exercices

---
+++ Test/Test.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//
+//class A {
+//public:
+//    A() { cout << "A"; }
+//    ~A() { cout << "a"; }
+//};
+//
+//int main() {
+//    A a;
+//    return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int x = 0;
+//    cout << (x++ == 0) << " " << x << endl;
+//    return 0;
+//}
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s = "hello";
+    s += '!';
+    cout << s.length() << endl;
+    return 0;
+}
