3eee420095cc81c2080b640a25a113d81b6a6749
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-05-08 16:47:47 +0300

    Brackets validator - interview task

---
+++ BracketsValidator/Program.cs
@@ -0,0 +1,37 @@
+﻿char[] chars = Console.ReadLine()!
+    .ToArray();
+
+Console.WriteLine(IsBracketsValid(chars));
+
+bool IsBracketsValid(char[] chars)
+{
+    Dictionary<char, char> openClosedBrackets = new()
+    {
+        {'(', ')' },
+        {'{', '}' },
+        {'[', ']' },
+        {'<', '>' },
+    };
+
+    Stack<char> brackets = new();
+    for (int i = chars.Length - 1; i >= 0; i--)
+    {
+        if (openClosedBrackets.ContainsKey(chars[i]))
+        {
+            if (brackets.Any() && brackets.Peek() == openClosedBrackets[chars[i]])
+            {
+                brackets.Pop();
+            }
+            else
+            {
+                brackets.Push(chars[i]);
+            }
+        }
+        else
+        {
+            brackets.Push(chars[i]);
+        }
+    }
+
+    return !brackets.Any();
+}
